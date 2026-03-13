db47c14a2561b0c4d27892fe74ab667c3956dd56
Ghanashyam123 <adshyam68@gmail.com>
2026-03-13 14:52:17 +0545

    second student registration project updated

---
+++ project/dashboard.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+</head>
+<body>
+     <h2>Admin Dashboard</h2>
+
+      <button > Add new Record  </button>
+
+      <table border="2">
+        <tr>    
+            <th>SN</th>
+            <th>student's name </th>
+            <th> address </th>
+            <th>Action </th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Sita karki </td>
+            <td>KTM </td>
+            <td>
+                <button>Edit </button>
+                <button>Delete</button>
+            </td>
+        </tr>
+      </table>
+         
+</body>
+</html>
---
+++ project/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+</head>
+<body>
+    <form method="post" action="">
+       <fieldset>
+        <legend>Login </legend>
+         <input type="text" name="username" name="username" placeholder="Enter username"/>
+                  <input type="password" name="password" name="username" placeholder="enter password"/>
+                           <input type="submit" name="login" value="Login"/>
+
+
+       </fieldset>
+
+    </form>
+</body>
+</html>
